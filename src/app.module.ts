a9ddea6efe3d508952d377c1825382812dc20cf8
sachin jat <53407215+sachinjat2802@users.noreply.github.com>
2023-07-16 23:00:51 +0530

    added products

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MongodbModule } from './mongodb/mongodb.module';
 import { UsersModule } from './users/users.module';
 import { GraphqlModule } from './graphql/graphql.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [MongodbModule, UsersModule, GraphqlModule],
+  imports: [MongodbModule, UsersModule, GraphqlModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
